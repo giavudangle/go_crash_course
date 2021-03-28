b44d0568e81228c50b05484ed1335c7631293423
Vudang <danglegiavu@gmail.com>
2021-03-28 21:21:40 +0700

    maps

--- go_crash_course/08_maps/main.go
+++ go_crash_course/08_maps/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	emails := make(map[string]string)
+	emails["vudang"] = "vudangdev@gmail.com"
+	emails["vudangcompany"] = "codingwithvudang@gmail.com"
+
+	fmt.Println(len(emails))
+	fmt.Println(emails["vudang"])
+	fmt.Println(len(emails["vudang"]))
+	delete(emails, "vudang")
+	fmt.Println(emails)
+
+	cars := map[string]int64{"Mercedes": 12000, "Mazada": 5000, "RollRoyces": 99999}
+
+	fmt.Println(cars)
 
 }
